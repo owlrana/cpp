56a17dcd910c75208ffd9e015040a91c6d2de2ab
Rahul Rana <me.rahulrana@outlook.com>
2020-11-14 23:58:51 +0530

    height checker

--- rahulrana/leetcode/#002 Running sum of 1D Array/v1.1.py
+++ rahulrana/leetcode/#002 Running sum of 1D Array/v1.1.py
@@ -1,9 +1,10 @@
-def runningSum(num_list: List[num]):
-    nums_sum = []
-    current_sum = 0
-    for item in nums:
-        current_sum += int(item)
-        nums_sum.append(current_sum)
-    return print(nums_sum)
+# Leetcode: https://leetcode.com/problems/running-sum-of-1d-array/submissions/
 
-nums = [1,2,3,4,5]
+class Solution:
+    def runningSum(self, nums) :
+        nums_sum = []
+        current_sum = 0
+        for item in nums:
+            current_sum += int(item)
+            nums_sum.append(current_sum)
+        return nums_sum
--- rahulrana/leetcode/#003 Height Checker/v1.0.py
+++
@@ -1,3 +0,0 @@
-# Non-Decreasing way to stand students
-# https://leetcode.com/problems/height-checker/
-# To be done.
