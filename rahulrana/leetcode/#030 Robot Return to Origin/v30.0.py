59cc782c0780333923945df4913fd1b40ad44057
Rahul Rana <me.rahulrana@outlook.com>
2020-12-04 00:34:18 +0530

    updated

--- rahulrana/leetcode/#030 Robot Return to Origin/v30.0.py
+++ rahulrana/leetcode/#030 Robot Return to Origin/v30.0.py
@@ -1,23 +1,20 @@
 # https://leetcode.com/problems/robot-return-to-origin/
 
 def judgeCircle(moves):
-    x, y = 0, 0
+    x = y = 0
     for char in moves:
         if char == 'U':
-            if y != 1:
-                y += 1
+            y += 1
         elif char == 'D':
-            if y != -1:
-                y -= 1
+            y -= 1
         elif char == 'L':
-            if x != -1:
-                x -= 1
+            x -= 1
         else:
-            if x != 1:
-                x += 1
-    if x == 0 and y == 0:
-        return x, y
-    return x, y
+            x += 1
+    return x == y == 0
 
 moves = "DRLLDLLRURLURULLLRULLRLULLLDDUDLUUUDLLDLDRLDRURDURRLRDLDRDLDDURDUURLLUUURLDRLUULUUDRDRUDURLLRDRRDLDU"
 print(judgeCircle(moves))
+
+# 60ms; faster than 56%
+# 14.4MB; less than 30#
