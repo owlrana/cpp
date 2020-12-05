74f781842d6b6e428c1439864f5c306d5ef21c5b
Rahul Rana <me.rahulrana@outlook.com>
2020-12-05 16:26:52 +0530

    updated

--- rahulrana/leetcode/#033 Next Greater Element I/v33.0.py
+++ rahulrana/leetcode/#033 Next Greater Element I/v33.0.py
@@ -3,32 +3,20 @@
 # git check
 
 def nextGreaterElement(nums1, nums2):
-    nextGE = []
-    for i in range(len(nums1)):
-        print('this loop for', nums1[i])
-        if max(nums2) <= nums1[i]:
-            print(max(nums2), 'is less than or equal to ', nums1[i])
-            nextGE.append(-1)
-            print("Exit IF")
-        else:
-            print("entered else")
-            mini = max(nums2)
-            for j in range(i-1, len(nums2)):
-                print("entered for")
-                if nums2[j] < mini and nums2[j] > nums1[i]and j > i:
-                    print("entered if")
-                    nextGE.append(nums2[j])
-                    break
-                if len(nums2) == i:
-                    break
-            if nums1[i] < mini:
-                nextGE.append(mini)
-            else:
-                nextGE.append(-1)
-    return nextGE
+    next_greater = []
+    for num in nums1:
+        start = nums2.index(num)
+        for j in range(start, len(nums2)):
+            if nums2[j] > num:
+                next_greater.append(nums2[j])
+                break
+        if len(next_greater) < nums1.index(num) + 1:
+            next_greater.append(-1)
+    return next_greater
 
-nums1 = [4,1,2] 
-nums2 = [1,3,4,2]
+nums1 = [4, 1, 2] 
+nums2 = [1,2,3,4]
 print(nextGreaterElement(nums1, nums2))
 
-# output -1, 3, -1
+# 88ms; faster than 14%
+# 14.4MB; less than 37%
