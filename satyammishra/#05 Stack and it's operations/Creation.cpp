012d8d4200a84e82cbc1853d1c0ad8c732a92286
Satyam <satyam2507mishra@gmail.com>
2020-12-05 17:05:57 +0530

    Initial commit to this rep

---
+++ satyammishra/#05 Stack and it's operations/Creation.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+struct stack {
+    int size;
+    int top;
+    int * s;
+}
+t;
+
+using namespace std;
+
+int main() {
+    cout << "Enter the size of the stack: ";
+    cin >> t.size;
+    t.s = new int[t.size];
+    return 0;
+    t.top =-1;
+}
