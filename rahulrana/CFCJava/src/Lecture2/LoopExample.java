ee5321500a5bb7b2c4e438fa1fd3b0f793ed7a93
Rahul Rana <me.rahulrana@outlook.com>
2021-02-02 18:21:39 +0530

    Lecture 3 mid-way

--- rahulrana/CFCJava/src/Lecture2/LoopExample.java
+++
@@ -1,9 +0,0 @@
-package Lecture2;
-
-public class LoopExample {
-    public static void main(String[] args) { // use psvm as shortcut
-        for (int i = 0; i < 15; i++) { // use fori as shortcut
-            System.out.println("Hello Rana");
-        }
-    }
-}
---
+++ rahulrana/CFCJava/src/com/competitive/Lecture2/SimpleInterest.java
@@ -0,0 +1,16 @@
+package com.competitive.Lecture2;
+
+public class SimpleInterest {
+
+    public static void main(String[] args) {
+
+        int p = 10000;
+        int r = 10;
+        int t = 1;
+
+        int si = (p * r * t) / 100;
+
+        System.out.println(si);
+        System.out.println("This is the interest");
+    }
+}
