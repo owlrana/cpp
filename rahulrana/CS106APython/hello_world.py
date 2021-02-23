6f1dbc2c34335b136e17e2823c4bba591ce357fb
Rahul Rana <me.rahulrana@outlook.com>
2021-02-23 18:08:14 +0530

    updated with final solutions

---
+++ rahulrana/CS106APython/hello_world.py
@@ -0,0 +1,10 @@
+# https://www.youtube.com/watch?v=wEbmXvfl8TM&feature=emb_logo
+
+# First Python Program, always put comments for the aim of your program + Functions
+
+def main():
+    print("Hello, World!")
+
+# This line is required in the end of a python file to call the main() function
+if __name__ == '__main__':
+    main()
