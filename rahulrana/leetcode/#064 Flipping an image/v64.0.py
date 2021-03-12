023c7958747dc4d3ea81debd1c89a1690e46450a
Rahul Rana <me.rahulrana@outlook.com>
2021-03-13 01:14:54 +0530

    updated

--- rahulrana/leetcode/#064 Flipping an image/v64.0.py
+++ rahulrana/leetcode/#064 Flipping an image/v64.0.py
@@ -1,22 +1,23 @@
 # https://leetcode.com/problems/flipping-an-image/
 
-
-def flipAndInvertImage(lst):
-    n = len(lst)
-    print(lst)
-    lstx = []
-    
+def horizontal(lst, n):
     # Flip Horizontally
+    lstx = []
     for i in range(n-1, -1, -1):
         lstx.append(lst[i].reverse())
     lstx = lst
-    print(lstx)
+    return lstx
     
+def flip(lst, n):
     # Flip Zeroes
-    for item in lst:
+    for j in range(n):
         for i in range(n):
-            item[i] = item[i]+1 - item[i]*2
-    print(lst)
-  
+            lst[j][i] = lst[j][i]+1 - lst[j][i]*2
+    return lst
+
+def flipAndInvertImage(lst):
+    n = len(lst)
+    return flip(horizontal(lst, n), n)
+
 # flipAndInvertImage([[1,1,0],[1,0,1],[0,0,0]])
 assert flipAndInvertImage([[1,1,0],[1,0,1],[0,0,0]]) == [[1,0,0],[0,1,0],[1,1,1]]
