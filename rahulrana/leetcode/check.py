fbb741dae7718256d787e5011dd67355d938fe29
Rahul Rana <me.rahulrana@outlook.com>
2021-03-16 23:56:24 +0530

    cleaned

--- rahulrana/leetcode/check.py
+++ rahulrana/leetcode/check.py
@@ -1,24 +1,2 @@
 # Check file
 
-class Solution:
-    def largeGroupPositions(s):
-        n = len(s)
-        if n < 3:
-            return []
-        intervals = []
-        i = 0
-        j = 1
-        count = 1
-        while i < n-1:
-            if s[i] == s[j]:
-                count += 1
-                j += 1
-            else:
-                if count >= 3:
-                    intervals.append([i,j-1])
-                i = j
-                j += 1
-                count = 1
-        return intervals
-
-print(Solution.largeGroupPositions("aa"))
