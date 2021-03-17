22f2ef85e3b6a0b55c27d3e3958372600cad6107
owlrana <me.rahulrana@outlook.com>
2021-03-18 02:28:46 +0530

    updated

--- rahulrana/leetcode/#055Cells with Odd Values in a Matrix.py
+++ rahulrana/leetcode/#055Cells with Odd Values in a Matrix.py
@@ -3,7 +3,28 @@
 class Solution:
     def oddCells(n: int, m: int, indices):
         # Create matrix of size MxN 
-        matrix = [[0]*m]*n
+        matrix = []
+        for i in range(n):
+            lst = []
+            for j in range(m):
+                lst.append(0)
+            matrix.append(lst)
+
         print(matrix)
 
-Solution.oddCells(2, 3, [[0,1], [1,1]])
+        for index in indices:
+            row = index[0]
+            col = index[1]
+            for i in range(n):
+                matrix[i][col] += 1
+            for i in range(m):
+                matrix[row][i] += 1
+        print(matrix)
+        count = 0
+        for i in range(n):
+            for j in range(m):
+                if matrix[i][j] % 2 != 0:
+                    count += 1
+        return count 
+
+print(Solution.oddCells(2, 3, [[0,1], [1,1]]))
