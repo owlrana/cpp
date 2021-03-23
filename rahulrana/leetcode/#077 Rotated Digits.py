bc69fae8ee3e727de87650059108cd2351f8236f
owlrana <me.rahulrana@outlook.com>
2021-03-24 00:00:57 +0530

    updated

---
+++ rahulrana/leetcode/#077 Rotated Digits.py
@@ -0,0 +1,36 @@
+def rotator(num):
+    valid = ['0', '1', '8', '2', '5', '6', '9']
+    validKeys = {'0': '0', '1': '1', '8': '8', '2': '5', '5': '2', '6': '9', '9': '6'}
+    if  num not in valid:
+        return False
+    else:
+        #print(num, "is being replaced by:", validKeys[num])
+        return validKeys[num]
+
+def checker(num):
+    numb = num
+    num = str(num)
+    num = list(num)
+    #print(num)
+    for i in range(len(num)):
+        if rotator(num[i]) == False:
+            return False
+        num[i] = rotator(num[i])
+    numStr = "".join(x for x in num)
+    #print(num)
+    num = int(numStr)
+    #print(numb, "was given, rotated to:", numStr)
+    return numStr
+    
+class Solution:
+    def rotatedDigits(N: int) -> int:
+        count = 0
+        for i in range(1, N+1):
+            if int(checker(i)) != i and int(checker(i)) != False:
+                #print(checker(i), " is not equal to input: ", i)
+                count += 1
+        return count
+
+in1 = input()
+in1 = int(in1)
+print(Solution.rotatedDigits(in1))
---
+++ rahulrana/leetcode/#078 Sum of Unique Elements.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/sum-of-unique-elements/submissions/
+
+# using " dict.get(n, 0) +1 "to initiate value of dict[n] by 0 or inc by 1 
+
+class Solution:
+    def sumOfUnique(self, nums):
+        freq = {}
+        for num in nums:
+            freq[num] = freq.get(num, 0) + 1
+        sum = 0
+        for key,value in freq.items():
+            if freq[key] == 1:
+                sum += int(key)
+        return sum
+
+#urls[url] = urls.get(url, 0) + 1
