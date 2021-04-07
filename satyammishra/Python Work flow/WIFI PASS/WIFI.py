2cb1d61fabe62293dff735446ef927c798f0da66
Satyam <satyam2507mishra@gmail.com>
2021-04-07 10:05:51 +0530

    WIFI

--- satyammishra/Python Work flow/WIFI PASS/WIFI.py
+++ satyammishra/Python Work flow/WIFI PASS/WIFI.py
@@ -1,13 +1,19 @@
 import subprocess 
 
+file_object = open('message.txt', 'w')
+id_pass = []
+
 a = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
 a = [i.split(":")[1][1:-1] for i in a if "All User Profile" in i]
 
 for i in a:
 	results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('utf-8').split('\n')
 	results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
+	
 	try:
-		print("{:<30}|  {:<}".format(i, results[0]))
+		#print("{:<30}|  {:<}".format(i, results[0]))
+		print(str(i) + " " + str(results[0]))
+
 	except IndexError:
 		print("{:<30}|  {:<}".format(i, ""))
 
