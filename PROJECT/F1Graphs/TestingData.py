385aaf36348b9f3078f584c40b4cdb963baa13f3
Devang Pandey <56135357+devang-7@users.noreply.github.com>
2021-04-28 22:30:36 +0530

    Version 1.1

--- PROJECT/F1Graphs/TestingData.py
+++ PROJECT/F1Graphs/TestingData.py
@@ -1,41 +1,29 @@
-# Library for opening url and creating requests
 import urllib.request
-# pretty-print python data structures
-from pprint import pprint
-
-# for converting the parsed data in a
-# pandas dataframe
-import pandas as pd
-# for parsing all the tables present on the website
 from html_table_parser import HTMLTableParser
-
-
-# Opens a website and read its
-# binary contents (HTTP Response Body)
+import ssl
+import pandas as pd
+import matplotlib.pyplot as plt
+from openpyxl.workbook import Workbook
+# Read website content
 def url_get_contents(url):
 
 	# Opens a website and read its
 	# binary contents (HTTP Response Body)
-
+    context = ssl._create_unverified_context() 
 	#making request to the website
-	req = urllib.request.Request(url=url)
-	f = urllib.request.urlopen(req)
+    req = urllib.request.Request(url=url)
+    f = urllib.request.urlopen(req, context=context)
+    return f.read()
 
-	#reading contents of the website
-	return f.read()
-
-# defining the html contents of a URL.
-xhtml = url_get_contents('https://www.racefans.net/2021/04/18/2021-emilia-romagna-grand-prix-interactive-data-lap-charts-times-and-tyres/').decode('utf-8')
-
-# Defining the HTMLTableParser object
-p = HTMLTableParser()
-
-# feeding the html contents in the HTMLTableParser object
-p.feed(xhtml)
-
-# Now finally obtaining the data of the table required
-pprint(p.tables[0])
-
-# converting the parsed data to dataframe
-#print("\n\nPANDAS DATAFRAME\n")
-print(pd.DataFrame(p.tables[0]))
+# return racing data table
+def dataInitialisation():
+    xhtml = url_get_contents('https://www.racefans.net/2021/04/18/2021-emilia-romagna-grand-prix-interactive-data-lap-charts-times-and-tyres/').decode('utf-8')
+    p = HTMLTableParser()
+    p.feed(xhtml)
+    raceData = p.tables[1]
+    
+    df = pd.DataFrame(raceData)
+    df.to_csv("output.csv")  
+    df_racingdata = pd.read_csv("output.csv")
+    print(df_racingdata)
+dataInitialisation()
